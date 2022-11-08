d155e517446acb8f083b8713272585d96a1766e0
ViniciusVitorio <vini.vtj@gmail.com>
2022-11-07 21:07:18 -0300

    add scroll up function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const themeToggler  = () => {
     const element = document.body;
     element.classList.toggle("light");
 }
+
+const scrollUp = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+document.addEventListener('scroll', () => {
+    const buttonScrollUp = document.getElementById('go_up_button');
+
+    buttonScrollUp.style.display = (window.scrollY >= 100) ? 'flex' : 'none';
+});
